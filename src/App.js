fe72c2e1a15aa73985366b738b0760cdc31c741e
100JOBISRAEL <kuchmarov@icloud.com>
2024-09-22 12:55:17 +0300

    New

--- src/App.js
+++ src/App.js
@@ -1,127 +1,104 @@
-import React, { useState } from 'react';
-import Lottie from 'react-lottie';
+import React, { useState, useEffect } from 'react';
+import Lottie from 'lottie-react';
+import taxiAnimation from './assets/taxi.json';
 import './App.css';
-import logo1 from './assets/logo1.png';
-import tickets from './assets/tickets.png';
-import duckAnimation from './assets/duckcard.json';
-import loadingAnimation from './assets/animation.json';
-
-const cardsData = Array.from({ length: 9 }, (_, i) => ({ id: i + 1 }));
 
 const App = () => {
-  const [showAnimation, setShowAnimation] = useState(true);
-  const [cards, setCards] = useState(cardsData);
-  const [flippedCards, setFlippedCards] = useState([]);
-  const [attemptsLeft, setAttemptsLeft] = useState(3);
-  const [cardsVisible, setCardsVisible] = useState(false);
-  const [fadeOut, setFadeOut] = useState(false);
-  const [showAttemptsOver, setShowAttemptsOver] = useState(false);
+  const [taxiCount, setTaxiCount] = useState(0);
+  const [tonCount, setTonCount] = useState(0);
+  const [isMining, setIsMining] = useState(false);
+  const [miningStartTime, setMiningStartTime] = useState(0);
 
-  // Function to handle play
-  const handlePlay = () => {
-    setFadeOut(true);
-    setTimeout(() => {
-      setShowAnimation(false);
-      setCardsVisible(true);
-    }, 1000); // Ensure this matches the fade-out duration
-  };
+  useEffect(() => {
+    const storedTaxiCount = parseFloat(localStorage.getItem('taxiCount')) || 0;
+    const storedTonCount = parseFloat(localStorage.getItem('tonCount')) || 0;
+    const storedMiningStartTime = parseInt(localStorage.getItem('miningStartTime')) || 0;
 
-  const handleCardClick = (cardId) => {
-    if (flippedCards.length < 3 && !flippedCards.includes(cardId) && attemptsLeft > 0) {
-      const newCards = cards.map(card => {
-        if (card.id === cardId) {
-          return { ...card, isFlipped: true, coinAmount: generateRandomCoins() };
-        }
-        return card;
-      });
+    setTaxiCount(storedTaxiCount);
+    setTonCount(storedTonCount);
 
-      setCards(newCards);
-      setFlippedCards([...flippedCards, cardId]);
-      setAttemptsLeft(attemptsLeft - 1);
+    // Проверяем, идет ли майнинг
+    if (storedMiningStartTime) {
+      const elapsedTime = Math.floor((Date.now() - storedMiningStartTime) / 1000);
+      const totalTime = 8 * 60 * 60; // 8 часов в секундах
+      const taxiPerSecond = 10000 / totalTime * 2;
+      const tonPerSecond = 0.001 / totalTime * 2;
+
+      const newTaxiCount = Math.min(storedTaxiCount + Math.floor(elapsedTime * taxiPerSecond), 10000);
+      const newTonCount = Math.min(storedTonCount + (elapsedTime * tonPerSecond), 0.001);
+
+      setTaxiCount(newTaxiCount);
+      setTonCount(newTonCount);
+    }
+  }, []);
+
+  useEffect(() => {
+    let taxiInterval;
+    let tonInterval;
+
+    if (isMining) {
+      const totalTime = 8 * 60 * 60; // 8 часов в секундах
+      const taxiPerSecond = 10000 / totalTime * 2;
+      const tonPerSecond = 0.001 / totalTime * 2;
+
+      taxiInterval = setInterval(() => {
+        setTaxiCount(prev => {
+          const newCount = Math.min(prev + taxiPerSecond, 10000);
+          localStorage.setItem('taxiCount', newCount);
+          return newCount;
+        });
+      }, 500);
+
+      tonInterval = setInterval(() => {
+        setTonCount(prev => {
+          const newCount = Math.min(prev + tonPerSecond, 0.001);
+          localStorage.setItem('tonCount', newCount);
+          return newCount;
+        });
+      }, 500);
+
+      setMiningStartTime(Date.now());
+      localStorage.setItem('miningStartTime', Date.now());
+
+      setTimeout(() => {
+        setIsMining(false);
+        clearInterval(taxiInterval);
+        clearInterval(tonInterval);
+        localStorage.removeItem('miningStartTime'); // Убираем время майнинга после окончания
+      }, totalTime * 1000);
     }
 
-    if (attemptsLeft === 1) {
-      setShowAttemptsOver(true);
-    }
-  };
+    return () => {
+      clearInterval(taxiInterval);
+      clearInterval(tonInterval);
+    };
+  }, [isMining]);
 
-  const generateRandomCoins = () => {
-    const coins = [1, 2, 3, 5];
-    return coins[Math.floor(Math.random() * coins.length)];
-  };
-
-  const totalCoins = cards.reduce((sum, card) => sum + (card.isFlipped ? card.coinAmount || 0 : 0), 0);
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: duckAnimation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
-  const loadingOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: loadingAnimation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
+  const startMining = () => {
+    setIsMining(true);
   };
 
   return (
-    <div className="App">
-      {showAnimation && (
-        <div
-          className={`animation-container ${fadeOut ? 'fade-out' : ''}`}
-          onClick={handlePlay}
-        >
-          <Lottie 
-            options={loadingOptions} 
-            height={200} 
-            width={200} 
-            isClickToPauseDisabled={true} 
-          />
-          <div className="tap-text">Tap me please...</div>
+    <div className="app">
+      <div className="counters">
+        <div className="counter">
+          <h2>$TAXI: {taxiCount.toFixed(2)}</h2>
         </div>
-      )}
-      <div className={`main-content ${showAnimation ? 'hidden' : ''}`}>
-        {showAttemptsOver && (
-          <div className="attempts-over">All attempts are used up!</div>
-        )}
-        {cardsVisible && (
-          <div className="status-bar">
-            <div className="status-item">
-              <img src={logo1} alt="Total Ton" className="status-icon" />
-              <span className="status-value">{totalCoins}</span>
-            </div>
-            <div className="status-item">
-              <img src={tickets} alt="Attempts Left" className="status-icon" />
-              <span className="status-value">{attemptsLeft}</span>
-            </div>
-          </div>
-        )}
-        <div className="card-grid-container">
-          <div className="card-grid">
-            {cards.map(card => (
-              <div
-                key={card.id}
-                className={`card ${card.isFlipped ? 'flipped' : ''}`}
-                onClick={() => handleCardClick(card.id)}
-              >
-                {card.isFlipped && (
-                  <div className="card-back">
-                    <p>{card.coinAmount || 0}</p>
-                  </div>
-                )}
-                {!card.isFlipped && (
-                  <Lottie options={defaultOptions} height={30} width={30} />
-                )}
-              </div>
-            ))}
-          </div>
+        <div className="counter">
+          <h2>TON: {tonCount.toFixed(6)}</h2>
         </div>
+      </div>
+      <div className="animation-container">
+        <Lottie 
+          animationData={taxiAnimation} 
+          loop 
+          style={{ width: '60%', height: 'auto' }} 
+        />
+      </div>
+      <div className="button-container">
+        <button className="button" onClick={startMining} disabled={isMining}>Start a working day</button>
+        <button className="button" disabled={!isMining}>Stop Mining</button>
+        <button className="button" disabled={!isMining}>Check Earnings</button>
       </div>
     </div>
   );
